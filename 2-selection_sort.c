4167bf83cf7eda7d5c8049bafb29101c3b488803
Zetrayo <josefeliciano.com98@gmail.com>
2024-08-09 15:40:00 +0200

    FIX : fixed betty and iso 90

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,28 +6,30 @@
  * @array: array of ints
  * @size: size of array
  */
-void selection_sort(int *array, size_t size) {
-    if (array == NULL || size < 2)
-        return;
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+	int temp;
 
-    for (size_t i = 0; i < size - 1; i++) {
-        size_t min_idx = i;
+	if (array == NULL || size < 2)
+		return;
 
-        // Find the index of the minimum element in the remaining unsorted array
-        for (size_t j = i + 1; j < size; j++) {
-            if (array[j] < array[min_idx]) {
-                min_idx = j;
-            }
-        }
-
-        // Swap the found minimum element with the first element of the unsorted array
-        if (min_idx != i) {
-            int temp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = temp;
-
-            // Print the array after each swap
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+			{
+				min_idx = j;
+			}
+		}
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
 }
