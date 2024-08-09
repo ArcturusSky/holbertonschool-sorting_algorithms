77f0b374555dc4fb4cf3feffe3c1139359f21e1d
Zetrayo <josefeliciano.com98@gmail.com>
2024-08-09 15:29:42 +0200

    FIX : 2-selection_sort.c, fixed code

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,27 +6,28 @@
  * @array: array of ints
  * @size: size of array
  */
-void selection_sort(int *array, size_t size)
-{
-	size_t i, j;
-	int tmp;
+void selection_sort(int *array, size_t size) {
+    if (array == NULL || size < 2)
+        return;
 
-	i = 0;
-	j = 1;
-	while (i < size - 1)
-	{
-		while (j <= size)
-		{
-			if (array[i] > array[j])
-			{
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
-			}
-			j++;
-		}
-		i++;
-		j = i + 1;
-		print_array(array, size);
-	}
+    for (size_t i = 0; i < size - 1; i++) {
+        size_t min_idx = i;
+
+        // Find the index of the minimum element in the remaining unsorted array
+        for (size_t j = i + 1; j < size; j++) {
+            if (array[j] < array[min_idx]) {
+                min_idx = j;
+            }
+        }
+
+        // Swap the found minimum element with the first element of the unsorted array
+        if (min_idx != i) {
+            int temp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = temp;
+
+            // Print the array after each swap
+            print_array(array, size);
+        }
+    }
 }
